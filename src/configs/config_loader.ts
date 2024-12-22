cad0665f9ce6d6584706a2cade0077cb0db2b77f
KIRIL-RA <ravel.kirill@gmail.com>
2024-12-22 03:45:30 +0300

    Bug fix

--- src/configs/config_loader.ts
+++ src/configs/config_loader.ts
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const isDev = true;
+const isDev = false;
 
 const _config = isDev ? JSON.parse(fs.readFileSync(path.join(path.dirname(__dirname), './configs/config.json'))) : JSON.parse(fs.readFileSync(path.join(path.dirname(process.execPath), './configs/config.json')));
 const keys = isDev ? JSON.parse(fs.readFileSync(path.join(path.dirname(__dirname), './configs/keys.json'))) : JSON.parse(fs.readFileSync(path.join(path.dirname(process.execPath), './configs/keys.json')));
